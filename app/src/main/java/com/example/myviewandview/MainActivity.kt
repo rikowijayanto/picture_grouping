b68b44c7c224f8fa80329cc6f1e1c70a073bc3a6
Riko Wijayanto <mr.rikowijayanto@gmail.com>
2020-04-12 01:19:50 +0700

    Adding description

--- app/src/main/java/com/example/myviewandview/MainActivity.kt
+++ app/src/main/java/com/example/myviewandview/MainActivity.kt
@@ -6,6 +6,11 @@ import androidx.appcompat.app.ActionBar
 
 class MainActivity : AppCompatActivity() {
 
+    //penambahan package implementation 'de.hdodenhof:circleimageview:3.0.0' di build.gradle
+    // penggunaan layout scroll view agar aplikasi bisa di scroll
+    // penggunaan table row untuk menampilkan spesifikasi
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
